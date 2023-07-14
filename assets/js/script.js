085030a6f71926883c51c8d06a1e0bf81e21a9bb
Jerammy Calbe <calbe.jerammy@gmail.com>
2023-07-15 04:45:36 +0800

    Changes styles on navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,13 @@
 // Variables
 const toggleButton = document.querySelector("#toggleButton");
-const navbar = document.querySelector(".navbar")
+const navbar = document.querySelector(".navbar");
 const navItems = document.querySelectorAll(".navbar ul li")
 const preLoader = document.querySelector(".preloader");
-const largeNavItems = document.querySelectorAll(".large-nav")
-
-const lightBg = document.querySelectorAll("#hero, #services, #reviews, #projects, #about, #collaborate");
-
+const largeNavItems = document.querySelectorAll(".large-nav");
+const lightBg = document.querySelectorAll(".navbar, #hero, #services, #reviews, #projects, #about, #collaborate, .offcanvas");
 const semiLightBg = document.querySelectorAll("hero, .card-body, #contact, #about .about-box");
-
-const blackText = document.querySelectorAll(".toggleIcon, .section-title, #services h1, #services h2, #projects h1, #projects .project-card .card-title, .tech-stack-text, .about-project-title, .number, #contact a h1, .quick-links a, .social-icons a");
-
-const paragraphs = document.querySelectorAll("#services p, #projects .project-card .card-text, .about-text, #contact p")
+const blackText = document.querySelectorAll(".toggleIcon, .section-title, .navbar .large-nav li a, .sub-nav li a, #services h1, #services h2, #projects h1, #projects .project-card .card-title, .tech-stack-text, .about-project-title, .number, #contact a h1, .quick-links a, .social-icons a, .btn-close");
+const paragraphs = document.querySelectorAll("#services p, #projects .project-card .card-text, .about-text, #contact p");
 
 
 // Dark and Light Mode
@@ -57,16 +53,14 @@ paragraphs.forEach((subTXT) => {
 });
 
 // Set header background color if scroll position is changed
-document.addEventListener("scroll", () => {
-    let scrollPosition = window.scrollY;
-    if (scrollPosition > 150) {
-        navbar.style.backgroundColor = "#212529";
-    } else {
-        navbar.style.background = "transparent";
-    }
-})
+// document.addEventListener("scroll", () => {
+//     let scrollPosition = window.scrollY;
+//     if (scrollPosition === 0) {
+//         navbar.style.background = "transparent";
+//     };
+// });
 
 // Preloader
 window.addEventListener("load", () => {
     preLoader.style.display = "none";
-})
+});
